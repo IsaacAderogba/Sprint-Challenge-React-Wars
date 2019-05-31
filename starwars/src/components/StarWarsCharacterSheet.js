de0afc0b9581531db9770c1cce4326be094fe424
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-05-31 08:58:58 +0100

    Add descriptive pieces for each character

--- starwars/src/components/StarWarsCharacterSheet.js
+++ starwars/src/components/StarWarsCharacterSheet.js
@@ -1,14 +1,41 @@
-import React from 'react';
-import './StarWars.css';
-
+import React from "react";
+import "./StarWars.css";
 
 class StarWarsCharacterSheet extends React.Component {
+  render() {
+    const {
+      name,
+      birth_year,
+      gender,
+      hair_color,
+      eye_color,
+      films,
+      height,
+      homeworld,
+      mass,
+      skin_color,
+      species,
+      starships,
+      vehicles
+    } = this.props.starwarsChar;
 
-    render() {
-        return (
-            <li className='StarWarsCharacterSheet'>{this.props.starwarsChar.name}</li>
-        );
-    }
+    const getDetails =
+      gender !== "n/a"
+        ? ` and is a ${gender} with ${hair_color} hair and ${eye_color} eyes`
+        : "";
+
+    return (
+      <li className="StarWarsCharacterSheet">
+        <h2>{name}</h2>
+        <p>
+          {name} was born in the year {birth_year}
+          {getDetails}.
+        </p>
+      </li>
+    );
+  }
 }
 
 export default StarWarsCharacterSheet;
+
+// {this.props.starwarsChar.name}
