6c193b31087d81391d2be08ae9bf039b491e4759
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-05-31 09:31:16 +0100

    Create CharacterStats component

--- starwars/src/components/StarWarsCharacterSheet.js
+++ starwars/src/components/StarWarsCharacterSheet.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./StarWars.css";
+import StarWarsCharacterStats from './StarWarsCharacterStats'
 
 class StarWarsCharacterSheet extends React.Component {
   render() {
@@ -31,6 +32,11 @@ class StarWarsCharacterSheet extends React.Component {
           {name} was born in the year {birth_year}
           {getDetails}.
         </p>
+        <div className='CharacterStatsContainer'>
+            <StarWarsCharacterStats data={height} dataHeading={'Height'} />
+            <StarWarsCharacterStats data={mass} dataHeading={'Mass'} />
+            <StarWarsCharacterStats data={skin_color} dataHeading={'Skin Color'} />
+        </div>
       </li>
     );
   }
